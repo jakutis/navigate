dc42560d558b51c341ca5d50c3236a7edbc67b17
Vytautas Jakutis <vytautas@jakut.is>
2012-10-02 20:08:47 +0300

    Revert "fix initialization on ie6"
    This reverts commit 697494641c439d9db79999d6b44505a532505a4c.

--- navigate.js
+++ navigate.js
@@ -204,7 +204,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
                         w.setTimeout(function() {
                             navigate(getCurrentPath());
                         }, 0);
-                    } else if(path !== '/favicon.ico') {
+                    } else {
                         w.location.href = opts.basePath + '/#' + path;
                     }
                 }
